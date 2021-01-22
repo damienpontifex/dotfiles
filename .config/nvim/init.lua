5ac1cd622d9e7e9d9992c803ba5f8233b1d35e45
Damien Pontifex <damien.pontifex@gmail.com>
2021-01-22 16:42:43 +0800

    reordering for shiftwidth settings applied

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,7 @@
 vim.api.nvim_command('source $HOME/.config/nvim/viminit.vim')
-require'options'
 require'keyboard'
 require'statusline'
 require'treesitter'
 require'lsp_setup'
 require'nerdtree'
-
+require'options'
