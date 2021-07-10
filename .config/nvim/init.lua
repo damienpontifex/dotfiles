433cd7718978715c03ec41adb1af7d9b732f711f
Damien Pontifex <damien.pontifex@gmail.com>
2021-07-10 18:46:07 +0800

    move plugin manager to packer with lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,46 @@
-local config_path = vim.fn.stdpath('config')
-vim.cmd('source ' .. config_path .. '/plugins.vim')
-vim.cmd('source ' .. config_path .. '/keyboard.vim')
+local packer_install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  os.execute('git clone https://github.com/wbthomason/packer.nvim ' .. packer_install_path)
+  vim.cmd 'packadd packer.nvim'
+end
+
+require'packer'.startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use 'scrooloose/nerdtree'
+  -- use 'ryanoasis/vim-devicons'
+  -- use 'tiagofumo/vim-nerdtree-syntax-highlight'
+  use 'tpope/vim-fugitive'
+
+  use 'tomasiser/vim-code-dark'
+  use 'arcticicestudio/nord-vim'
+  -- use 'morhetz/gruvbox'
+  use 'chriskempson/base16-vim'
+  use 'vim-airline/vim-airline'
+
+  use { 'junegunn/fzf', run = function() vim.cmd'fzf#install()' end }
+  use 'junegunn/fzf.vim'
+  use 'mileszs/ack.vim'
+
+  use 'neovim/nvim-lspconfig'
+  use 'nvim-lua/completion-nvim'
+  use 'hashivim/vim-terraform'
+  use 'pprovost/vim-ps1'
+  use 'plasticboy/vim-markdown'
+  -- use 'rust-lang/rust.vim'
+  -- use 'Yggdroot/indentLine'
+  use 'editorconfig/editorconfig-vim'
+
+  use 'nvim-lua/popup.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'
+  
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+end)
+
 vim.cmd [[
   syntax enable
   set t_ut= " without this line, weird things happen when using tmux
@@ -14,4 +54,4 @@ vim.cmd [[
 
   colorscheme base16-gruvbox-dark-hard
 ]]
-vim.cmd('source ' .. config_path .. '/viminit.vim')
+
