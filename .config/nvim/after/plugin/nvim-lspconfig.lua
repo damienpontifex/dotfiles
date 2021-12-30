035e242d4251deea755658b770fa15c7eb35c137
Damien Pontifex <damien.pontifex@gmail.com>
2021-12-30 08:48:45 +0800

    updated lsp setup

--- .config/nvim/after/plugin/nvim-lspconfig.lua
+++ .config/nvim/after/plugin/nvim-lspconfig.lua
@@ -45,7 +45,9 @@ local function on_attach(client, bufnr)
 
   buf_set_keymap('n', '<leader>dn', '<cmd> lua vim.lsp.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', '<leader>dp', '<cmd> lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', '<leader>ds', '<cmd> lua vim.diagnostic.open_float()<CR>', opts)
   buf_set_keymap('n', '<leader>af', '<cmd> lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', '<leader>kf', '<cmd> lua vim.lsp.buf.formatting()<CR>', opts)
 
   -- Set some keybinds conditional on server capabilities
   if client.resolved_capabilities.document_formatting then
@@ -61,26 +63,12 @@ local function on_attach(client, bufnr)
     buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
   end
 
-  -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec([[
-      " hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
-      " hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
-      " hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
-      " augroup lsp_document_highlight
-      "   autocmd! * <buffer>
-      "   autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-      "   autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      " augroup END
-    ]], false)
-  end
-
   -- vim.api.nvim_command('autocmd CursorHold * lua vim.lsp.util.show_line_diagnostics()')
 end
 
 -- Use a loop to conveniently both setup defined servers
 -- and map buffer local keybindings when the language server attaches
-local servers = {} -- {'jsonls', 'gopls', 'pyls', 'cmake', 'omnisharp', 'rust_analyzer', 'sumneko_lua', 'tsserver'}
+local servers = {'jsonls', 'gopls', 'cmake', 'omnisharp', 'rust_analyzer', 'sumneko_lua', 'tsserver'}
 for _, lsp in ipairs(servers) do
   lsp_config[lsp].setup {
     on_attach = on_attach,
@@ -188,8 +176,9 @@ function update_rust_analyzer()
     echo "Rust analyzer version:" \
     $(curl --silent https://api.github.com/repos/rust-analyzer/rust-analyzer/releases/latest \
     | jq --raw-output '.name') \
-    && curl --location --silent --output /usr/local/bin/rust-analyzer \
-      https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-mac  \
+    && curl --location --silent \
+      https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-$(uname -m)-apple-darwin.gz \
+      | gunzip > /usr/local/bin/rust-analyzer  \
     && chmod +x /usr/local/bin/rust-analyzer
   ]])
 end
