63ffb4a594ffd66ca2c67554d2e524a85109a8a9
Damien Pontifex <damien.pontifex@gmail.com>
2025-07-06 12:41:25 +0800

    codecompanion status

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -3,6 +3,18 @@ return {
     'github/copilot.vim'
   },
   {
+    'ravitemer/mcphub.nvim',
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+    opts = {
+      log = {
+        level = vim.log.levels.INFO,
+      },
+    },
+  },
+  {
     "olimorris/codecompanion.nvim",
     opts = {
       system_prompt = function()
@@ -15,6 +27,16 @@ return {
         handle:close()
         return out
       end,
+      extensions = {
+        mcphub = {
+          callback = 'mcphub.extensions.codecompanion',
+          opts = {
+            show_results_in_chat = true,
+            make_vars = true,
+            make_slash_commands = true,
+          },
+        },
+      },
     },
     dependencies = {
       "nvim-lua/plenary.nvim",
@@ -27,33 +49,34 @@ return {
     config = function(_, opts)
       require('codecompanion').setup(opts)
 
-      -- local progress = require('fidget.progress')
-      -- local handles = {}
-      -- local group = vim.api.nvim_create_augroup('CodeCompanionHooks', { clear = true })
-      --
-      -- vim.api.nvim_create_autocmd('User', {
-      --   pattern = 'CodeCompanionChatSubmitted',
-      --   group = group,
-      --   callback = function(e)
-      --     handles[e.data.id] = progress.handle.create({
-      --       title = 'CodeCompanion',
-      --       message = 'Thinking...',
-      --       lsp_client = { name = e.data.adapater.formatted_name },
-      --     })
-      --   end
-      -- })
-      --
-      -- vim.api.nvim_create_autocmd({ 'CodeCompanionChatStopped', 'CopeCompanionChatCleared' }, {
-      --   group = group,
-      --   callback = function(e)
-      --     local handle = handles[e.data.id]
-      --     if handle then
-      --       handle.message = e.data.status == "success" and 'Done!' or 'Error!'
-      --       handle:finish()
-      --       handles[e.data.id] = nil
-      --     end
-      --   end
-      -- })
+      local progress = require('fidget.progress')
+      local handles = {}
+      local group = vim.api.nvim_create_augroup('CodeCompanionFidgetHooks', { clear = true })
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'CodeCompanionRequestStarted',
+        group = group,
+        callback = function(e)
+          handles[e.data.id] = progress.handle.create({
+            title = " Requesting assistance (" .. e.data.strategy .. ")",
+            message = 'In progress...',
+            lsp_client = { name = e.data.adapter.formatted_name },
+          })
+        end
+      })
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = "CodeCompanionRequestFinished",
+        group = group,
+        callback = function(e)
+          local handle = handles[e.data.id]
+          if handle then
+            handle.message = e.data.status == "success" and 'Done!' or 'Error!'
+            handle:finish()
+            handles[e.data.id] = nil
+          end
+        end
+      })
     end
   },
 }
