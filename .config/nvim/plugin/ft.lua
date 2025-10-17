8f29ad995013fcff9db97eac635e694ed48c4394
Damien Pontifex <damien.pontifex@gmail.com>
2025-10-17 17:09:11 +0800

    local brewfile packages

--- .config/nvim/plugin/ft.lua
+++ .config/nvim/plugin/ft.lua
@@ -1,11 +1,13 @@
 vim.filetype.add({
 	filename = {
-		[".Brewfile"] = "ruby",
 		[".env"] = "sh",
 		[".envrc"] = "sh",
 		["*.env"] = "sh",
 		["*.envrc"] = "sh",
 		["*.env.*"] = "sh",
+	},
+	pattern = {
+		["Brewfile.*"] = "ruby",
 	},
 	extension = {
 		bicep = "bicep",
