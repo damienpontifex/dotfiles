83f8bc71177ae773ae733895211ed99dec3b5f02
Damien Pontifex <damien.pontifex@gmail.com>
2025-11-07 11:22:22 +0800

    comment keymaps builtin to nvim as of 0.12

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,3 +1,8 @@
-return {
-	"numToStr/Comment.nvim",
-}
+if vim.fn.has("nvim-0.12") == 1 then
+	-- Built in to 0.12 https://github.com/neovim/neovim/pull/28176
+	return {}
+else
+	return {
+		"numToStr/Comment.nvim",
+	}
+end
