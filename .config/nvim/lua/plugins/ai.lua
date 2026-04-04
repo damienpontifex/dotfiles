768e2675b82cb2401d186ecdc15a489a27bdfe84
Damien Pontifex <damien.pontifex@gmail.com>
2026-04-05 07:20:07 +0800

    Use copilot from language server and inline completion

--- .config/nvim/lua/plugins/ai.lua
+++
@@ -1,5 +0,0 @@
-vim.pack.add({ "https://github.com/github/copilot.vim" })
-
-vim.api.nvim_set_keymap("i", "<M-CR>", 'copilot#Accept("<CR>")', { expr = true, noremap = true, silent = true })
-vim.g.copilot_no_tab_map = true
-vim.g.copilot_workspace_folders = { vim.fn.getcwd() }
