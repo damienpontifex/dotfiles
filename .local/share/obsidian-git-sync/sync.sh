1db1f2a045ec05be0966a2be98ec89dd3df31b91
Damien Pontifex <damien.pontifex@gmail.com>
2026-08-26 08:57:02 +0800

    host in commit message

--- .local/share/obsidian-git-sync/sync.sh
+++ .local/share/obsidian-git-sync/sync.sh
@@ -14,6 +14,6 @@ git pull origin "$branch" --rebase
 
 if [[ -n $(git status --porcelain) ]]; then
   git add .
-  git commit -m "Automated vault sync: $(date +'%Y-%m-%d %H:%M:%S')"
+  git commit -m "Automated vault sync: $(date +'%Y-%m-%d %H:%M:%S') from $HOST"
   git push origin "$branch"
 fi
