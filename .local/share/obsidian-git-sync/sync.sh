9712f0f514a13155e3057abdda95acfdbe3911a6
Damien Pontifex <damien.pontifex@gmail.com>
2026-08-26 08:54:56 +0800

    Make obsidian sync script work across machines for different vault locations

--- .local/share/obsidian-git-sync/sync.sh
+++ .local/share/obsidian-git-sync/sync.sh
@@ -2,12 +2,18 @@
 
 set -euo pipefail
 
-cd "$HOME/Library/Mobile Documents/iCloud~md~obsidian/Documents/ponti"
+log_dir="$HOME/.local/state/obsidian-git-sync"
+mkdir -p "$log_dir"
+exec >>"$log_dir/sync.log" 2>&1
+exec >>"$log_dir/sync.log" 2>&1
 
-git pull origin main --rebase
+cd ~/.obsidian-vault
+
+branch=$(git symbolic-ref --short HEAD)
+git pull origin "$branch" --rebase
 
 if [[ -n $(git status --porcelain) ]]; then
   git add .
   git commit -m "Automated vault sync: $(date +'%Y-%m-%d %H:%M:%S')"
-  git push origin main
+  git push origin "$branch"
 fi
